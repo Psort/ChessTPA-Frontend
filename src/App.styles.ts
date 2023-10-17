0bf95324123153ed72f0f12c8155c7e31a41f42b
Psort <skowronpiotr.00@gmail.com>
2023-10-17 22:20:19 +0200

    add: Section refactor: AppContainer

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-    min-height: 100vh;
+    height: 100vh;
+    display: flex;
+    flex-direction: row-reverse;
+  @media(max-width: 960px){
+    flex-direction: column;
+  }
+`
+export const Section = styled.div`
+    width: 90vw;
+    height: 100vh;
+  @media(max-width: 960px){
+    width: 100vw;
+    height: 90vh;
+  }
 `
