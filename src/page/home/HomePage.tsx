b36f6e401cd006f8f8ed850b2e38f83d23d96a5c
Psort <skowronpiotr.00@gmail.com>
2023-10-17 22:21:21 +0200

    refactor

--- src/page/home/HomePage.tsx
+++ src/page/home/HomePage.tsx
@@ -1,8 +1,10 @@
+import {Section} from "../../App.styles";
+import React from "react";
 
 export const HomePage = () =>{
     return(
-        <>
-            Home
-        </>
+        <Section>
+            Whereas disregard and contempt for human rights have resulted
+        </Section>
     )
 }
