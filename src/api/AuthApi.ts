4500422e20603a0cf5d86a604b44487e6400ab0b
Psort <skowronpiotr.00@gmail.com>
2023-10-17 22:20:39 +0200

    add: login

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import {SignInRequest} from "../model/api/SignInRequest";
-import {SignInResponse} from "../model/api/SignInResponse";
+import {LoginResponse} from "../model/api/LoginResponse";
+
+class LoginRequest {
+}
 
 export class AuthApi {
   static signIn = async (request: SignInRequest) =>
-    await axios.post<SignInResponse>("auth/register", request);
+    await axios.post("auth/register", request);
 
 
+  static login = async (request: LoginRequest) =>
+      await axios.post<LoginResponse>("auth/login", request);
 }
