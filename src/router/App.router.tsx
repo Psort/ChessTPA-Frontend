50332bbe923efee330e94332e86e44553a1c6538
Psort <skowronpiotr.00@gmail.com>
2023-10-17 22:19:56 +0200

    add: SetContainer

--- src/router/App.router.tsx
+++ src/router/App.router.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import {HomePage} from "../page/home/HomePage";
 import {LoginPage} from "../page/login/LoginPage";
+import {SetContainer} from "../components/SetContainer";
 export const AppRouter = ()=>{
     return(
         <Routes>
+            <Route element={<SetContainer/>}>
             <Route path="/" element={<HomePage/>}></Route>
             <Route path="/login" element={<LoginPage/>}></Route>
+            </Route>
         </Routes>
     )
 }
