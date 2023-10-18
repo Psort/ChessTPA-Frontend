8d3127e1896854dadfc967b488a0bf3387b1562f
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-18 13:51:41 +0200

    add: Signup page to router

--- src/router/App.router.tsx
+++ src/router/App.router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import {HomePage} from "../page/home/HomePage";
 import {LoginPage} from "../page/login/LoginPage";
 import {SetContainer} from "../components/SetContainer";
+import {SignupPage} from "../page/signup/SignupPage";
 export const AppRouter = ()=>{
     return(
         <Routes>
             <Route element={<SetContainer/>}>
             <Route path="/" element={<HomePage/>}></Route>
             <Route path="/login" element={<LoginPage/>}></Route>
+            <Route path="/signup" element={<SignupPage/>}></Route>
             </Route>
         </Routes>
     )
