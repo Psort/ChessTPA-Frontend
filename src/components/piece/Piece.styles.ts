a82e1e34b9287dd2c0054a291d71bbbed396b370
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-23 17:51:09 +0200

    add: Piece component

---
+++ src/components/piece/Piece.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledPiece = styled.img`
+    height: 90%;
+  aspect-ratio: 1;
+    `
---
+++ src/components/piece/Piece.tsx
@@ -0,0 +1,8 @@
+import {BoardContainer} from "../board/Board.styles";
+import React from "react";
+import {StyledPiece} from "./Piece.styles";
+export const Piece = () =>{
+    return (
+        <StyledPiece src={"king.png"}/>
+    );
+}
