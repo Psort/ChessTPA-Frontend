8b67b94e2ace19ad566538c05f7ed24bfaec7781
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-24 15:57:00 +0200

    add: ChessSquare component

---
+++ src/components/board/ChessSquare.tsx
@@ -0,0 +1,33 @@
+import {useDrop} from "react-dnd";
+import {PiecesTypes} from "../../model/pieces/PieceType";
+import {StyledChessSquare} from "./Board.styles";
+import React, {useContext} from "react";
+import {movePiece} from "../piece/Piece";
+import {GameContext} from "../../context/GameContext";
+
+type ChessSquareProps = {
+    x: number,
+    y: number,
+    color: string,
+    children?: React.ReactNode
+}
+export const ChessSquare = (props: ChessSquareProps) => {
+    const context = useContext(GameContext)
+    const [ { isOver, canDrop },drop] = useDrop({
+        accept: PiecesTypes.KNIGHT,
+        drop: () => movePiece(props.x, props.y, context.currentPiece),
+        canDrop: () => true,
+        collect: monitor => ({
+            isOver: monitor.isOver,
+            canDrop: monitor.canDrop
+        })
+    });
+
+    return (
+        <div ref={drop}>
+        <StyledChessSquare x={props.x} y={props.y} color={props.color}>
+            {props.children}
+        </StyledChessSquare>
+        </div>
+    )
+}
