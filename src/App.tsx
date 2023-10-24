e3647e5f8a4285d5cd9bceb56648f068a03db18f
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-24 15:59:19 +0200

    add: BoardContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { AppContainer } from "./App.styles";
 import {AppRouter} from "./router/App.router";
 import {withAxiosIntercepted} from "./hooks/withAxiosIntercepted";
+import {GameContextProvider} from "./context/GameContext";
 
 function App() {
   return (
+      <GameContextProvider>
         <AppContainer>
             <AppRouter/>
         </AppContainer>
+      </GameContextProvider>
   );
 }
 
