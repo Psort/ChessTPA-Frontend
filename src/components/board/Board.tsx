83d6e66e76e8607072f6d9fe15c4b9f00f85e81f
Psort <skowronpiotr.00@gmail.com>
2023-10-25 14:57:16 +0200

    add: GameContext.tsx: move pieces canMove Board.tsx: GameContext

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,25 +1,26 @@
 import {BoardContainer, StyledChessSquare} from "./Board.styles";
-import React, {useEffect} from "react";
+import React, {useContext, useEffect} from "react";
 import {Piece} from "../piece/Piece";
-import {pieces} from "../../model/pieces/Pieces";
+import {deafoultPiecesSetUp} from "../../model/pieces/DeafoultPiecesSetUp";
 import {ChessSquare} from "./ChessSquare";
+import {GameContext} from "../../context/GameContext";
 
 export const Board = () => {
-    const spots: React.ReactElement[] = Array.from({ length: 64 }, (_, index) => {
+    const gameContext = useContext(GameContext)
+    const spots: React.ReactElement[] = Array.from({length: 64}, (_, index) => {
         const i = Math.floor(index / 8) + 1;
         const j = index % 8 + 1;
         const color = (i + j) % 2 === 0 ? "white" : "black";
-        const piece = pieces.find(piece => piece.x === i && piece.y === j);
+        const piece = gameContext.pieces.find(piece => piece.x === i && piece.y === j);
         return (
             <ChessSquare x={i} y={j} color={color} key={`${i}-${j}`}>
-                {piece && <Piece color={piece.color} type={piece.type} x={piece.x} y={piece.y} />}
+                {piece && <Piece color={piece.color} type={piece.type} x={piece.x} y={piece.y}/>}
             </ChessSquare>
         );
     });
-
     return(
         <BoardContainer >
-                    {spots}
+            {spots}
         </BoardContainer>
     )
 }
