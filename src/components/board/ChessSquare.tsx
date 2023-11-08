2f36a4ec1cb4bd418990e58425121507c1faea77
Psort <skowronpiotr.00@gmail.com>
2023-11-08 15:57:21 +0100

    add ChessSquare.tsx: move canMove Piece.tsx: setPossibleMoves board

--- src/components/board/ChessSquare.tsx
+++ src/components/board/ChessSquare.tsx
@@ -1,7 +1,7 @@
 import {useDrop} from "react-dnd";
 import {PiecesTypes} from "../../model/pieces/PieceType";
 import {StyledChessSquare} from "./Board.styles";
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {GameContext} from "../../context/GameContext";
 
 type ChessSquareProps = {
@@ -12,19 +12,57 @@ type ChessSquareProps = {
 }
 export const ChessSquare = (props: ChessSquareProps) => {
     const context = useContext(GameContext)
+    const [isPossibleMove,setIsPossibleMove] = useState(false)
     const [ { isOver, canDrop },drop] = useDrop({
-        accept: PiecesTypes.KNIGHT,
-        drop: () => context.move(props.x,props.y),
-        canDrop: () => context.canMove(props.x,props.y),
+        accept: context.currentPiece?.type??"",
+        drop: () => move(props.x,props.y),
+        canDrop: () => canMove(props.x,props.y),
         collect: monitor => ({
             isOver: monitor.isOver,
-            canDrop: monitor.canDrop
+            canDrop: monitor.canDrop,
         })
     });
+    const move = (x: number, y: number) => {
+        if (context.currentPiece) {
+            const updatedPieces = context.pieces.map(piece =>
+                (piece.x === context.currentPiece?.x && piece.y === context.currentPiece.y)
+                    ? { ...piece, x, y } : piece
+            );
+
+            let updatedCurrentPiece = { ...context.currentPiece }; // Create a mutable copy
+
+            if (context.currentPiece.type === PiecesTypes.PAWN && context.promoteX === x) {
+                updatedCurrentPiece = { ...context.currentPiece, type: PiecesTypes.QUEEN };
+            }
+
+            const pieceIndex = updatedPieces.findIndex(piece => piece.x === x && piece.y === y);
+            if (pieceIndex !== -1) {
+                updatedPieces[pieceIndex].color = updatedCurrentPiece.color;
+                updatedPieces[pieceIndex].type = updatedCurrentPiece.type;
+            }
+
+            context.piecesModifier(updatedPieces.filter(piece => piece.x !== updatedCurrentPiece.x || piece.y !== updatedCurrentPiece.y));
+            context.colorTurnModifier(context.colorTurn == "white"?"black":"white")
+        }
+    };
+    function canMove(x: number, y: number) {
+
+        if (context.currentPiece?.color != context.colorTurn){
+            return false
+        }
+        if(context.possibleMoves){
+            return !!context.possibleMoves?.find(move => move.x === x && move.y+1 === y);
+        }
+        return false
+    }
+    useEffect(() => {
+        const hasPossibleMoves= context.possibleMoves?!!context.possibleMoves?.find(move => move.x === props.x && move.y+1 === props.y):false
+        setIsPossibleMove(hasPossibleMoves);
+    }, [context.possibleMoves]);
 
     return (
         <div ref={drop}>
-        <StyledChessSquare x={props.x} y={props.y} color={props.color}>
+        <StyledChessSquare x={props.x} y={props.y} color={props.color} isPossibleMove={isPossibleMove}>
             {props.children}
         </StyledChessSquare>
         </div>
--- src/components/piece/Piece.tsx
+++ src/components/piece/Piece.tsx
@@ -1,30 +1,54 @@
 
-import React, {useContext, useEffect} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import {Img, StyledPiece} from "./Piece.styles";
 import {PieceModel} from "../../model/pieces/PieceModel";
 import {  useDrag } from "react-dnd";
-import {PiecesTypes} from "../../model/pieces/PieceType";
 import {GameContext} from "../../context/GameContext";
+import {convertPosition, generateChessboard} from "../../utils/GameContextUtils";
+import {GameApi} from "../../api/GameApi";
 
 export const Piece = (piece:PieceModel) =>{
     const context = useContext(GameContext)
+    const[board,setBoard] = useState("")
     const [{ isDragging }, drag] = useDrag({
-        type: PiecesTypes.KNIGHT, // Ensure piece.type is of type PieceType
-        item: { type: PiecesTypes.KNIGHT},
+        type: piece.type, // Ensure piece.type is of type PieceType
+        item: { type: piece.type},
         collect: monitor => ({
             isDragging: monitor.isDragging()
         })
     });
+    const setPossibleMoves = useCallback(async () =>{
+        try {
+
+            const response = await GameApi.getPossibleMoves({
+                boardState: board,
+                piecePosition: convertPosition(context.currentPiece?.x, context.currentPiece?.y)
+            });
+            context.possibleMovesModifier(response.data)
+            // console.log(response.data)
+        } catch (error) {
+            // console.log(error)
+        }
+        return true;
+    },[board,context])
+    useEffect(() => {
+        if (board){
+            setPossibleMoves()
+        }
+    }, [board,context.currentPiece]);
     useEffect(() => {
         if (isDragging){
             context.currentPieceModifier(piece)
+            setBoard(generateChessboard(context.pieces))
+        }else{
+            context.possibleMovesModifier(null)
         }
     }, [isDragging]);
     return (
         <StyledPiece ref = {drag}
                      style={{
                          opacity: isDragging ? 0.5 : 1
-                     }}>
+                     }} >
             <Img src={require("../../resources/Img/Pieces/"+piece.type+piece.color+".png")}/>
         </StyledPiece>
     );
