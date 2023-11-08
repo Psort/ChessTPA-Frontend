10a78a16e39cce8cedbe2a2981066bedda33365f
Psort <skowronpiotr.00@gmail.com>
2023-11-08 15:51:42 +0100

    Add AuthApi.ts: +refreshToken

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -2,13 +2,17 @@ import axios from "axios";
 import {SignInRequest} from "../model/api/auth/SignInRequest";
 import {LoginResponse} from "../model/api/auth/LoginResponse";
 import {LoginRequest} from "../model/api/auth/LoginRequest";
+import {RefreshTokenRequest} from "../model/api/auth/RefreshTokenRequest";
 
 
 export class AuthApi {
   static signIn = async (request: SignInRequest) =>
     await axios.post("auth/register", request);
 
-
   static login = async (request: LoginRequest) =>
       await axios.post<LoginResponse>("auth/login", request);
+
+  static refreshToken = async (request: RefreshTokenRequest) =>
+      await axios.post<LoginResponse>("auth/refresh", request);
+
 }
