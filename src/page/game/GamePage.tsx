f5c8d4a4400373267ec60bf863f5ab4812ab9938
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-14 16:22:04 +0100

    change GamePage: receive gameId from param

--- src/page/game/GamePage.tsx
+++ src/page/game/GamePage.tsx
@@ -3,15 +3,17 @@ import React, {useCallback, useContext, useEffect} from "react";
 import {Board} from "../../components/board/Board";
 import {GameApi} from "../../api/GameApi";
 import {GameContext} from "../../context/GameContext";
+import {useParams} from "react-router-dom";
 
 
-export const GamePage = () =>{
+export const GamePage = () => {
+    const { gameId } = useParams();
     const gameContext = useContext(GameContext)
 
     const game = useCallback(async () => {
         try {
-            const response = await GameApi.getGame(gameContext.game?.id)
-            console.log(response.data)
+            const response = await GameApi.getGame(gameId)
+            gameContext.gameModifier(response.data)
 
         } catch (error: any) {
             console.log(error)
