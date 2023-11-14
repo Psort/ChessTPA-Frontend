57c18a40f870544c06db14124bc0a274acadc828
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-14 16:16:26 +0100

    change GameApi: getPossibleMoves request type

--- src/api/GameApi.ts
+++ src/api/GameApi.ts
@@ -1,13 +1,11 @@
 import {authorizedApi} from "../hooks/withAxiosIntercepted";
-import {GameRequest} from "../model/api/game/GameRequest";
 import {GameResponse} from "../model/api/game/GameResponse";
+import {MovesRequest} from "../model/api/game/MovesRequest";
 
 
 export class GameApi {
-  // static getPossibleMoves = async (request: GameRequest) =>
-  //   await authorizedApi.get<GameResponse[]>("/engine",{params:request} );
-  static getPossibleMoves = async (request:GameRequest) =>
-      await authorizedApi.post("/game/move",request);
+  static getPossibleMoves = async (request: MovesRequest) =>
+      await authorizedApi.post("/engine/move", request);
   static getGame = async (gameId:string|undefined) =>
       await authorizedApi.get<GameResponse>(`/game?gameId=${gameId}`);
 }
