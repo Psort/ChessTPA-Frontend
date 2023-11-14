12391d5638d935b5f713810d191eceb61241e35e
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-14 16:25:03 +0100

    add Navbar: logout button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,19 @@
 
-import React from "react";
+import React, {useContext} from "react";
 import {LoginButton, Logo, NavContainer, RegisterButton} from "./Navbar.styles";
 import {useNavigate} from "react-router-dom";
+import {EMAIL} from "../../constants/constants";
+import {UserContext} from "../../context/UserContext";
 
 export const Navbar = () =>{
     const navigate = useNavigate();
+    const userContext = useContext(UserContext)
+
+    const logut = () => {
+        localStorage.removeItem(EMAIL);
+        userContext.userModifier(null);
+        navigate("/")
+    }
     return(
         <NavContainer>
             <Logo/>
@@ -17,7 +26,10 @@ export const Navbar = () =>{
             <LoginButton onClick={()=>navigate("/login")}>
                 Log in
             </LoginButton>
-
+            <LoginButton onClick={() => logut()
+            }>
+                Log out
+            </LoginButton>
         </NavContainer>
     )
 }
