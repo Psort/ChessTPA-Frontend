b34854ee91d818aff5326b9651231f65ff0dc023
Psort <skowronpiotr.00@gmail.com>
2023-11-15 14:45:32 +0100

    refactor : Board: spots

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -5,18 +5,17 @@ import {ChessSquare} from "./ChessSquare";
 import {GameContext} from "../../context/GameContext";
 
 export const Board = () => {
-    const gameContext = useContext(GameContext)
-    const spots: React.ReactElement[] = Array.from({length: 64}, (_, index) => {
-        const i = Math.floor(index / 8) + 1;
-        const j = index % 8 + 1;
-        const color = (i + j) % 2 === 0 ? "white" : "black";
-        const piece = gameContext.pieces.find(piece => piece.x === i && piece.y === j);
-        return (
-            <ChessSquare x={i} y={j} color={color} key={`${i}-${j}`}>
-                {piece && <Piece color={piece.color} type={piece.type} x={piece.x} y={piece.y}/>}
-            </ChessSquare>
-        );
-    });
+    const gameContext = useContext(GameContext);
+    let spots: React.ReactElement[] =gameContext.pieces.flatMap((row, i) => {
+            return row.map((piece, j) => {
+                const color = (i + j) % 2 === 0 ? "white" : "black";
+                return (
+                    <ChessSquare x={i+1} y={j+1} color={color} key={`${i}-${j}`}>
+                        {piece.type  && <Piece color={piece.color} type={piece.type} x={piece.x} y={piece.y} />}
+                    </ChessSquare>
+                );
+            });
+        });
     return(
         <BoardContainer >
             {spots}
