b9152381618b2b503b0186358812154fc3280c2a
Psort <skowronpiotr.00@gmail.com>
2023-11-15 14:48:43 +0100

    refactor : GamePage: getGame

--- src/page/game/GamePage.tsx
+++ src/page/game/GamePage.tsx
@@ -4,28 +4,29 @@ import {Board} from "../../components/board/Board";
 import {GameApi} from "../../api/GameApi";
 import {GameContext} from "../../context/GameContext";
 import {useParams} from "react-router-dom";
+import {boardStateToBoard} from "../../utils/GameContextUtils";
 
 
 export const GamePage = () => {
     const { gameId } = useParams();
     const gameContext = useContext(GameContext)
 
-    const game = useCallback(async () => {
+    const getGame = useCallback(async () => {
         try {
             const response = await GameApi.getGame(gameId)
             gameContext.gameModifier(response.data)
-
+            const boardState= response.data.history.at(response.data.history.length - 1)?.boardState;
+            if (boardState){
+                gameContext.piecesModifier(boardStateToBoard(boardState))
+            }
         } catch (error: any) {
             console.log(error)
         }
-    }, [gameContext.game?.id]);
+    }, []);
     useEffect(() => {
-        game()
+        getGame()
     }, []);
 
-    useEffect(() => {
-        console.log(gameContext.game)
-    }, [gameContext.game]);
     return(
         <Section>
            <Board/>
