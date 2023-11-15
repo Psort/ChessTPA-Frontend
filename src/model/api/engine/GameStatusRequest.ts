88e29d2b2583890502e54e766126597d3051f822
Psort <skowronpiotr.00@gmail.com>
2023-11-15 14:50:04 +0100

    add : GameStatusRequest GameStatusResponse

---
+++ src/model/api/engine/GameStatusRequest.ts
@@ -0,0 +1,5 @@
+export type GameStatusRequest = {
+    boardState:string
+    color:string
+    castles: string[]
+}
--- src/model/api/game/MovesRequest.ts
+++
@@ -1,5 +0,0 @@
-export type MovesRequest = {
-    boardState:string
-    piecePosition:string
-    castles: string[]
-}
