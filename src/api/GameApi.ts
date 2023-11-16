5ad8dc41e4874b492a4a2d366a29d8178caf15fd
Psort <skowronpiotr.00@gmail.com>
2023-11-16 15:29:59 +0100

    add: GameApi: safeGameStatus()

--- src/api/GameApi.ts
+++ src/api/GameApi.ts
@@ -1,10 +1,12 @@
 import {authorizedApi} from "../hooks/withAxiosIntercepted";
 import {GameResponse} from "../model/api/game/GameResponse";
-import {MovesRequest} from "../model/api/engine/MovesRequest";
+import {GameStateRequest} from "../model/api/game/GameStateRequest";
 
 
 export class GameApi {
   static getGame = async (gameId:string|undefined) =>
       await authorizedApi.get<GameResponse>(`/game?gameId=${gameId}`);
 
+  static safeGameStatus = async (request: GameStateRequest) =>
+      await authorizedApi.patch("/game", request);
 }
