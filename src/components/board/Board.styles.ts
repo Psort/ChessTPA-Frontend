832e69de29f2e5e5cb2a7b1e3257d325a45061a3
Psort <skowronpiotr.00@gmail.com>
2023-11-16 15:27:48 +0100

    refactor: Board.styles.ts: +add reverse view for black

--- src/components/board/Board.styles.ts
+++ src/components/board/Board.styles.ts
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
-export const BoardContainer = styled.div`
+export const BoardContainer = styled.div<{playerColor:string ;}>`
  display: grid;
  grid-template-columns: repeat(8, 1fr);
- grid-template-rows: repeat(8, 1fr);
+ grid-template-rows:  repeat(8, 1fr);
  width: fit-content;
  aspect-ratio: 1;
  position: absolute;
  top: 50%;
  left: 45%;
- transform: translate(-50%, -50%);
+ transform: translate(-50%, -50%) ${(props)=>props.playerColor==="BLACK" ? "rotate(180deg)" : ""};
  `
-export const StyledChessSquare = styled.div<{ x: number; y: number; color: string ;isPossibleMove:boolean}>`
+export const StyledChessSquare = styled.div<{ x: number; y: number; color: string;playerColor:string ;isPossibleMove:boolean}>`
  aspect-ratio: 1;
  width: 3rem;
  grid-column: ${(props) => props.y};
  grid-row: ${(props) => props.x};
  background:${(props) => props.isPossibleMove ? "red" :props.color};
+ transform: ${(props)=>props.playerColor==="BLACK" ? "rotate(180deg)" : ""};
 `;
