14924bc8768058c8c222eef2aef22cd69ec0e0d0
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-20 19:38:33 +0100

    change: disable sockjs logs

--- src/message/MessageSender.ts
+++ src/message/MessageSender.ts
@@ -3,6 +3,7 @@ import SockJS from "sockjs-client";
 
 export const sendMessageWithGameId = (gameId: string) => {
     const client = Stomp.over(new SockJS('http://localhost:8080/stomp'));
+    client.debug  = function (){}
     client.connect({}, () => {
         client.send(`/app/game/${gameId}`, {}, gameId);
         client.disconnect(() => {
