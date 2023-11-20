e813044f8b0f93ccb66876a92d832a632683f1cc
Psort <skowronpiotr.00@gmail.com>
2023-11-20 16:22:24 +0100

    refactor: ChessSquare: import {sendMessageWithGameId} from "../../message/MessageSender"; -> import {sendMessageWithGameId} from "../../utils/WebSocketUtils";

--- src/components/board/ChessSquare.tsx
+++ src/components/board/ChessSquare.tsx
@@ -7,7 +7,7 @@ import {EngineApi} from "../../api/EngineApi";
 import {boardToBoardState, convertPosition} from "../../utils/GameContextUtils";
 import {GameApi} from "../../api/GameApi";
 import {UserContext} from "../../context/UserContext";
-import {sendMessageWithGameId} from "../../message/MessageSender";
+import {sendMessageWithGameId} from "../../utils/WebSocketUtils";
 
 type ChessSquareProps = {
     x: number,
