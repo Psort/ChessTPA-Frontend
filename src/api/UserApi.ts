3cf644ba0ac2ae56c857f18ea3110be2e8117186
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-12-07 15:44:11 +0100

    add: calculateElo method

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,7 +1,10 @@
 import {authorizedApi} from "../hooks/withAxiosIntercepted";
 import {User} from "../model/User";
+import {EloTradeRequest} from "../model/api/user/EloTradeRequest";
 
 export class UserApi {
     static getUser = async (email:string | null) =>
         await authorizedApi.get<User>(`user?email=${email}`);
+    static calculateElo = async (request: EloTradeRequest) =>
+        await authorizedApi.patch("/user", request)
 }
