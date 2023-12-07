fb074cc38a099c08691bf369c59c7f88a35a81f7
Psort <skowronpiotr.00@gmail.com>
2023-12-07 16:02:11 +0100

    clean code

--- src/components/modal/EndGameModal.tsx
+++ src/components/modal/EndGameModal.tsx
@@ -10,13 +10,13 @@ interface EndGameModalProps {
 
 export const EndGameModal: React.FC<EndGameModalProps> = ({ isOpen, onClose }) => {
     const gameContext = useContext(GameContext)
-    const playerColor = gameContext.lastMove?.player?.color
+    const winnerPLayerColor = gameContext.lastMove?.player?.color
 
     return (
         <ModalWrapper isOpen={isOpen}>
             <ModalContent>
                 <h2>GAME OVER</h2>
-                <h3>{winnerColor} WINS</h3>
+                <h3>{winnerPLayerColor} WINS</h3>
                 <p>REMATCH</p>
                 <CloseButton onClick={onClose}>&times;</CloseButton>
             </ModalContent>
