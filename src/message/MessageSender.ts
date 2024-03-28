0accee66b7abece5836406e3b036a651ce9ac426
Psort <skowronpiotr.00@gmail.com>
2024-03-28 16:28:01 +0100

    refactor: websocket Refactor

--- src/message/MessageSender.ts
+++ src/message/MessageSender.ts
@@ -1,11 +1,21 @@
 import {Stomp} from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
-export const sendMessageWithGameId = (gameId: string) => {
+export const sendGameWithGameId = (gameId: string) => {
     const client = Stomp.over(new SockJS('http://localhost:8080/stomp'));
     client.debug  = function (){}
     client.connect({}, () => {
         client.send(`/app/game/${gameId}`, {}, gameId);
+        client.disconnect(() => {
+        });
+    });
+};
+
+export const sendMessageWithGameId = (gameId: string) => {
+    const client = Stomp.over(new SockJS('http://localhost:8080/stomp'));
+    client.debug  = function (){}
+    client.connect({}, () => {
+        client.send(`/app/messages/${gameId}`, {}, gameId);
         client.disconnect(() => {
         });
     });
